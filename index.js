9ac3279107b0015f7adf2ea4839e4969decb4070
Jetezh 123 <vegasetiawan16@gmail.com>
2024-05-15 03:10:18 +0000

    solution change

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const cors = require('cors');
 const { nanoid } = require('nanoid');
 
 app.use(cors());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 const dataUsers = [];
@@ -18,88 +18,106 @@ const dataUsers = [];
 app.post("/api/users", (req, res) => {
   const username = req.body.username;
 
-  if(!username) {
-    return res.json({message: "Please input username"})
-  };
+  if (!username) {
+    return res.json({ message: "Please input username" });
+  }
 
   const _id = nanoid(24);
   const newUser = {
     _id,
     username
-  }
+  };
 
   dataUsers.push(newUser);
-  
+
   res.json(newUser);
 });
 
 app.get("/api/users", (req, res) => {
   let dataUsersFiltered = [...dataUsers];
 
-  if(dataUsersFiltered === 0) {
-    return res.json({message: "There is no user"});
+  if (dataUsersFiltered.length === 0) {
+    return res.json({ message: "There is no user" });
   }
 
   res.json(dataUsersFiltered.map((user) => ({
-      _id: user._id,
-      username: user.username
-    }))
-  )
+    _id: user._id,
+    username: user.username
+  })));
 });
 
 app.post("/api/users/:_id/exercises", (req, res) => {
   const { date, duration, description } = req.body;
-  const _id = req.body[":_id"];
+  const { _id } = req.params;
   const index = dataUsers.findIndex((user) => user._id === _id);
 
-  if(index !== -1) {
-    if(description.length > 30) {
-      res.json({message: "Description too long"});
-    } else {
-      if(isNaN(duration))
-        return res.json({message: "duration must be a number"})
-
-      if(!dataUsers[index].exercises)
-        dataUsers[index].exercises = [];
-    
-      const newExercise = {
-        description,
-        duration, 
-        date: date ? new Date(date).toDateString() : new Date().toDateString()
-      }
-  
-      dataUsers[index].exercises.push(newExercise);
-  
-      res.json({
-        _id: dataUsers[index]._id,
-        username: dataUsers[index].username,
-        description: newExercise.description,
-        duration: newExercise.duration,
-        date: newExercise.date,
-      })
-
-      console.log(dataUsers);
+  if (index !== -1) {
+    if (typeof description !== 'string' || description.length > 30) {
+      return res.json({ message: "Description must be a string and no longer than 30 characters" });
     }
+
+    const durationNumber = parseInt(duration);
+    if (isNaN(durationNumber)) {
+      return res.json({ message: "Duration must be a number" });
+    }
+
+    if (!dataUsers[index].exercises) {
+      dataUsers[index].exercises = [];
+    }
+
+    const newExercise = {
+      description: description.toString(),
+      duration: durationNumber,
+      date: date ? new Date(date).toDateString() : new Date().toDateString(),
+    };
+
+    dataUsers[index].exercises.push(newExercise);
+
+    res.json({
+      _id: dataUsers[index]._id,
+      username: dataUsers[index].username,
+      description: newExercise.description,
+      duration: newExercise.duration,
+      date: newExercise.date,
+    });
   } else {
-    res.json({message: "user not found"});
+    res.json({ message: "User not found" });
   }
 });
 
 app.get("/api/users/:_id/logs", (req, res) => {
-  const _id = req.params[":_id"];
+  const { from, to, limit } = req.query;
+  const { _id } = req.params;
   const index = dataUsers.findIndex((user) => user._id === _id);
-  
-  if(index !== -1) {
-    const user = dataUsers[index];
+
+  if (index !== -1) {
+    let exercises = dataUsers[index].exercises || [];
+
+    if (from) {
+      const fromDate = new Date(from);
+      exercises = exercises.filter(exercise => new Date(exercise.date) >= fromDate);
+    }
+
+    if (to) {
+      const toDate = new Date(to);
+      exercises = exercises.filter(exercise => new Date(exercise.date) <= toDate);
+    }
+
+    if (limit) {
+      exercises = exercises.slice(0, parseInt(limit));
+    }
+
     res.json({
-      _id: user._id,
-      username: user.username,
-      count: user.exercises.length,
-      log: user.exercises
-    })
+      _id: dataUsers[index]._id,
+      username: dataUsers[index].username,
+      count: exercises.length,
+      log: exercises
+    });
+  } else {
+    res.json({ message: "User not found" });
   }
-})
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
